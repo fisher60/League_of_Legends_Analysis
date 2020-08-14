df6764f1f84020bdbb41fd52318cd053cf253d8e
harrisdurrani <32649675+harrisdurrani@users.noreply.github.com>
2020-08-14 16:00:15 -0700

    Update get_data.py
    added doc string to get_column

--- league_analysis/parsing/get_data.py
+++ league_analysis/parsing/get_data.py
@@ -2,6 +2,7 @@ from pandas import read_csv
 import numpy
 
 def get_column(name = []):
+    '''input list of column names, get dataframe with column names.'''
     csv = read_csv("league_analysis/datasets/high_diamond_ranked_10min.csv")
     return csv[[i for i in name]]
 
