81d8665040fb47d2a5f806271b20f5488cd3b884
Kyler <39353605+fisher60@users.noreply.github.com>
2020-08-14 16:09:58 -0700

    Replace keyword argument with expected type
    do not want default type, this would result in improper use not raising the proper exception and perhaps returning no data.

--- league_analysis/parsing/get_data.py
+++ league_analysis/parsing/get_data.py
@@ -1,7 +1,7 @@
 from pandas import read_csv
 import numpy
 
-def get_column(name = []):
+def get_column(name: list):
     '''input list of column names, get dataframe with column names.'''
     csv = read_csv("league_analysis/datasets/high_diamond_ranked_10min.csv")
     return csv[[i for i in name]]
