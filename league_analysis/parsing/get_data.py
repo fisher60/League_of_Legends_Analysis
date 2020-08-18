5ecae3e741ac09aaf7250025f4bcdd13a0e93ba2
Harris <harris.durrani@hotmail.com>
2020-08-18 09:43:01 -0700

    added path lib to resolve directory issues

--- league_analysis/parsing/get_data.py
+++ league_analysis/parsing/get_data.py
@@ -1,9 +1,11 @@
 from pandas import read_csv
 from league_analysis.settings import DATA_DIR
+from pathlib import Path
+
 
 
 def get_columns(names: list) -> list:
     """input list of column names, get dataframe with column names."""
 
-    csv = read_csv(f"{DATA_DIR}\high_diamond_ranked_10min.csv")
+    csv = read_csv(Path(DATA_DIR, "high_diamond_ranked_10min.csv"))
     return [csv[i] for i in names]
