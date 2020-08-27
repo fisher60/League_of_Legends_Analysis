71d4c222711aba85083db3e8144ee7dbbdfe59cd
Harris <harris.durrani@hotmail.com>
2020-08-27 15:12:47 -0700

    added knn analysis

--- league_analysis/__main__.py
+++ league_analysis/__main__.py
@@ -1,8 +1,11 @@
 from sys import argv
 from league_analysis.analysis.regression import vision_to_deaths
+from league_analysis.analysis.neigh import dragons_to_kills
 
 if __name__ == "__main__":
     command_1 = argv[1]
 
     if command_1 == "regression":
         vision_to_deaths()
+    elif command_1 == "neigh":
+        dragons_to_kills()
